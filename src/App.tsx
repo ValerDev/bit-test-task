f908a88a11afbbde728d2b74af8a9216a68c22bc
valerDev <valer.val7@gmail.com>
2024-01-14 22:04:15 +0400

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import s from './App.module.scss';
 import { Dashboard } from './components/Dashboard/Dashboard';
 import { UserInformationBar } from './components/UserInformationBar/UserInformationBar';
+import { useAppSelector } from './hooks/storeHooks';
 
 function App() {
-
+  const { userInformationBar } = useAppSelector(state => state.usersSlice)
   return (
-    <div className={s.app}>
-     <Dashboard/>
-     <UserInformationBar/>
+    <div className={s.app} style={userInformationBar ? {overflow: 'hidden'} : {}}>
+      <Dashboard />
+      <UserInformationBar />
     </div>
   );
 }
